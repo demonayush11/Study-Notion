e3a4c4a6a93b7952bcef2158eb0ff738e7c38bdb
demonayush11 <ayush.tanay2314@gmail.com>
2025-08-11 18:51:21 +0530

    added payment gateway

--- controllers/Payments.js
+++ controllers/Payments.js
@@ -66,6 +66,16 @@ exports.capturepayment =async (req,res) =>{
     try{
          const paymentResponse =await instance.orders.create(options);
          console.log(paymentResponse);
+         //return response
+         return res.status.json({
+            succes:true,
+            courseName:course.courseNmae,
+            courseDescription:course.courseDescription,
+            thumbnail:course.thumbnail,
+            orderId:paymentResponse.id,
+            currency:paymentResponse.currency,
+            amount:paymentResponse.amount,
+         });
     }
     catch(error){
        console.log(error);
@@ -74,6 +84,75 @@ exports.capturepayment =async (req,res) =>{
          message:"could not initiate the order"
        })
     } 
+
+    //verify signature of razorpay and server
+    exports.verifySignature =async(req,res) =>{
+         const webhookSecret ="12345678";
+
+         const signature =req.headers["x-razorpay-signature"];
+
+         const shasum =crypto.createHmac("sha256",webhookSecret);
+         shasum.update(JSON.stringify(req.body));
+         const digest =shasum.digest("hex");
+
+         if(signature===digest){
+            console.log("payment is authorised");
+
+            const {courseId,userId} =req.body.payload.payment.entity.notes;
+            
+            try{
+                 //fulfil the action
+
+                 //find the course and enroll the udent in it
+                 const enrollcourse = await Course.findOneAndUpdate(
+                    {_id:courseId},
+                    {$push:{studentsEnrolled: userId}},
+                    {new:true},
+                 );
+
+                 if(!enrollcourse){
+                    return res.status(500).json({
+                        success:false,
+                        message:"course not found",
+                    });
+                 }
+                 console.log(enrollcourse);
+
+                 //find the student and add the course to their list enroled courss me
+                 const enrolledStudent =await User.findOneAndupdate(
+                    {_id:userId},
+                    {$push:{course:courseId}},
+                    {new:true},
+                 );
+                 console.log(enrolledStudent);
+
+                 //mail send  for confirmation
+                 const emailResponse =await mailSender(
+                    enrolledStudent.email,
+                    "congratulation from StudyNotion",
+                    "Congratulatuon,you are onboarded into new study notion course",                 
+                );
+
+                console.log(emailResponse);
+                return res.status(200).json({
+                    success:true,
+                    message:"Signature Verified and Course added",
+                });
+            }
+            catch(error){
+               return res.status(500).json({
+                status:false,
+                message:  error.message,
+               })
+            }
+        }
+        else{
+            return res.status(400).json({
+                success:false,
+                message:"invalid request",
+            })
+        }
+    };
   }
   catch(error){
 
