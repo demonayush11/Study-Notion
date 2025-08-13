9fee8bf4caadfaa9ebe5eef9462501453c88551e
demonayush11 <ayush.tanay2314@gmail.com>
2025-08-13 16:15:44 +0530

    fixed bug

--- controllers/Category.js
+++ controllers/Category.js
@@ -39,17 +39,68 @@ exports.createCategory =async (req,res) =>{
 };
 
 //getAll tags handler function
-exports.showAllCategory =async (req,res) =>{
+exports.showAllCategories =async (req,res) =>{
     try{
            const allCategory =await Category.find({},  {name:true,description:true});
            res.status(200).json({
             success:true,
-            message:"all tags returned successfully",
+            message:"all categories returned successfully",
             allCategory, 
            })
     }
     catch(error){
          return res.status(500).json({
+            success:lfalse,
+            message:error.message,
+        })
+    }
+}
+//categorypagedetails
+exports.categoryPageDetails =async (req,res) =>{
+    try{
+       //get categoryId
+       const {categoryId}=req.body; 
+       //get courses for specified categoryId
+       const selectcategory =await Category.findById(categoryId)
+                                            .populate("courses")
+                                            .exec();
+       //validation
+       if(!selectcategory){
+        return res.status(404).json({
+            success:false,
+            message:'Data Not Found',
+        });
+       }
+       //get courses for different categories
+       const differentCategories =await Category.find({
+                                                 _id:{$ne:categoryId},                                          
+                                                  })
+                                                  .populate("courses")
+                                                  .exec();
+
+        //get top selling course
+        const allCategories = await Category.find()
+      .populate({
+        path: "courses",
+        match: { status: "Published" },
+      })
+      .exec()
+    const allCourses = allCategories.flatMap((category) => category.courses)
+    const mostSellingCourses = allCourses
+      .sort((a, b) => b.sold - a.sold)
+      .slice(0, 10)
+        //return response
+        return res.status(200).json({
+            success:true,
+            data:{
+                selectcategory,
+                differentCategories,
+                mostSellingCourses,
+            }
+        })
+    }
+    catch(error){
+        return res.status(500).json({
             success:lfalse,
             message:error.message,
         })
